066b517860295ac1b8ae985d8f4c1cf904fb57a6
Eryk Mika <erykmika108@gmail.com>
2026-08-07 22:18:18 +0200

    fix: expose ml model in dockerized api

--- api/docker-entrypoint.sh
+++ api/docker-entrypoint.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 set -e
 
+TRAINED_MODEL="${TRAINED_MODEL:-/training/models/poland_used_cars_linear_regression.joblib}"
+API_MODEL_DIR="${API_MODEL_DIR:-models}"
+
+echo "Copying trained model..."
+mkdir -p "$API_MODEL_DIR"
+cp "$TRAINED_MODEL" "$API_MODEL_DIR/poland_used_cars_linear_regression.joblib"
+
 echo "Running database migrations..."
 alembic upgrade head
 
